a81d9310c03514bfb4abf6da985466d5b78c558e
Shivangx01b <46749216+Shivangx01b@users.noreply.github.com>
2020-12-15 12:24:07 +0530

    Create recon.sh

---
+++ recon.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+
+installations() {
+
+	echo "[+] Echo intalling esentials"
+	sudo apt-get -y update
+	sudo apt-get install -y libcurl4-openssl-dev
+	sudo apt-get install -y libssl-dev
+	sudo apt-get install -y jq
+	sudo apt-get install -y ruby-full
+	sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+	sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+	sudo apt-get install -y python-setuptools
+	sudo apt-get install -y libldns-dev
+	sudo apt-get install -y python3-pip
+	sudo apt-get install -y python-pip
+	sudo apt-get install -y python-dnspython
+	sudo apt-get install -y git
+	sudo apt-get install -y rename
+	sudo apt-get install -y xargs
+	sudo apt-get install -y golang
+
+	echo "[+] Installing aquatone"
+	go get -u -v github.com/michenriksen/aquatone
+
+	echo "[+] Installing assetfinder"
+	go get -u  -v github.com/tomnomnom/assetfinder
+
+	go get -u -v github.com/caffix/amass
+
+	go get -u -v github.com/cgboal/sonarsearch/crobat
+
+
+
+}
+
+
+fetch_resolvers() {
+
+	wget https://raw.githubusercontent.com/Shivangx01b/Fresh-Dns/main/resolvers.txt -o /home/ubuntu/Recon/$1/resolvers.txt
+}
+
+Passive_Subdomain_Enumerations () {
+
+	amass enum -passive -silent -d  $1 -o "/tmp/amass_$1.txt"
+	/home/ubuntu/go/bin/assetfinder -subs-only $1 >> "/tmp/assetfinder_$1.txt"
+	/home/ubuntu/go/bin/crobat -s $1 >> "/tmp/crobat_$1.txt"
+	/home/ubuntu/go/bin/subfinder -d $1  -silent -o "/tmp/subfinder_$1.txt"
+	bash /home/ubuntu/subs.sh $1
+	cat "/tmp/amass_$1.txt" "/tmp/assetfinder_$1.txt" "/tmp/subfinder_$1.txt"  "/tmp/crobat_$1.txt" "/tmp/all_others_$1.txt" | sort -u  >> "/home/ubuntu/Recon/$1/$1_passive.txt"
+	rm "/tmp/amass_$1.txt" 
+	rm "/tmp/assetfinder_$1.txt" 
+	rm "/tmp/subfinder_$1.txt"  
+	rm "/tmp/crobat_$1.txt" 
+	rm "/tmp/all_others_$1.txt"
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Passive Enumeration for $1 is done"
+}
+
+resolve_subs() {
+
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Running subdomain resolvers"
+	echo "$1"
+	/home/ubuntu/go/bin/shuffledns -d $1 -list "/home/ubuntu/Recon/$1/$1_passive.txt" -r "/home/ubuntu/Recon/$1/resolvers.txt" -silent -o "/home/ubuntu/Recon/$1/$1_subdomains.txt"
+	rm "/home/ubuntu/$1_passive.txt"
+	count=$(cat "/home/ubuntu/Recon/$1/$1_subdomains.txt" | wc -l) 
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Found $count domains for $1 in passive mode"
+}
+
+
+takeover_checks() {
+
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Running checks for subdomain takeover"
+	/home/ubuntu/go/bin/takeover -l /home/ubuntu/Recon/$1/$1_subdomains.txt -p /home/ubuntu/go/src/github.com/mzfr/takeover/providers.json -t 80 >> "/home/ubuntu/Recon/$1/$1_sustakeover.txt"
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Finished subdomain takeover checks"
+}
+
+httprobes() {
+
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Running probes for http/https"
+	cat "/home/ubuntu/Recon/$1/$1_subdomains.txt" | /home/ubuntu/go/bin/httprobe -c 80 -p 80,443,8443,8043,8080,8081,8089 >> "/home/ubuntu/Recon/$1/$1_httprobe.txt"
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Finished httprobes"
+}
+
+nuclei_checks() {
+
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Running nuclei checks"
+	for i in $(cat "/home/ubuntu/tests.txt"); do /home/ubuntu/go/bin/nuclei -l "/home/ubuntu/Recon/$1/$1_subdomains.txt" -t /home/ubuntu/nuclei-templates/$i -silent >> "/home/ubuntu/Recon/$1/$1_nuclei.txt"; done
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Finished nuclei checks"
+}
+
+
+
+screenshots() {
+
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Taking screenshots for $1"
+	cat "/home/ubuntu/Recon/$1/$1_httprobe.txt" | /home/ubuntu/go/bin/go-stare -o "/home/ubuntu/Recon/$1/$1_screenshots"
+	tar cvf "/home/ubuntu/Recon/$1/$1_screenshots.tar" /home/ubuntu/Recon/$1/$1_screenshots
+	rm -rf "/home/ubuntu/Recon/$1/$1_screenshots"
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Screenshots finished for $1"
+}
+
+
+runner() {
+
+	$1=$(echo $start)
+	mkdir ~/Recon/$1
+	fetch_resolvers $1
+	Passive_Subdomain_Enumerations $1
+	resolve_subs $1
+	takeover_checks  $1
+	httprobes $1
+	nuclei_checks $1
+	screenshots $1
+	curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=================================================="
+
+}
+
+
+sleep 30 
+curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Starting ReconDelta"
+#installations
+for start in $(cat '/home/ubuntu/targets.txt'); do runner $start; done
+curl -s -X GET "https://api.telegram.org/bot1348630881:AAHkrJqzeux3uU42opMCKGv78ilGfsUBO64/sendMessage?chat_id=-415374781&parse_mode=Markdown&text=Finished ReconDelta"
